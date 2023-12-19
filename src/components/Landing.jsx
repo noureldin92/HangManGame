1f1287f10385505831fd91e0675b074310b0c205
moimenwagdy <moimenwy@gmail.com>
2023-12-19 20:55:33 +0200

    d

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <main className="h-screen my-[-32px]  bg-white rounded-full flex justify-center flex-col items-center  border-orange-600 w-3/4 mx-auto">
-      <h1 className="text-6xl font-extrabold text-orange-600">
+      <h1 className="text-4xl font-extrabold text-center text-orange-600">
         Welcome To HangMan Challenge
       </h1>
       <p className="mt-12">
